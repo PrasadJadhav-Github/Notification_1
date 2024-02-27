478122d6e4d67abd5d8241dbe0054d6a9f95f7f7
Prasad <prasadrjadhav11@gmail.com>
2024-02-27 10:51:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notification_1/UpdatesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notification_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpdatesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.updates_activity)
+    }
+}
